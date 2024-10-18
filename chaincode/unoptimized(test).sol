357e0c178d004a00ac30f0bfb35ba1d513502fd3
Takudzwa Choto <56199912+TakudzwaChoto@users.noreply.github.com>
2024-10-18 22:16:21 +0800

    initial commit

--- chaincode/unoptimized(test).sol
+++ chaincode/unoptimized(test).sol
@@ -1,52 +1,48 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
-                contract WaterQualityManagementUnoptimized {
-                    struct QualityEvent {
-                        uint256 timestamp;
-                        int256 qualityLevel;
-                    }
-                    mapping(address => QualityEvent[]) public 
-                    recentQualityRecords;
-                    mapping(address => QualityEvent[]) public 
-                    historicalQualityData;
+pragma solidity ^0.5.11;
 
-                    function reportQuality(address user, int256 
-                    qualityLevel) public {
-                        uint256 currentTimestamp = block.timestamp;
-                        QualityEvent memory newEvent = 
-                        QualityEvent(currentTimestamp, qualityLevel);
-                        recentQualityRecords[user].push(newEvent);
-                    }
+contract WaterQualityManagementUnoptimized {
+    struct QualityEvent {
+        uint256 timestamp;
+        int256 qualityLevel;
+    }
+    mapping(address => QualityEvent[]) public recentQualityRecords;
+    mapping(address => QualityEvent[]) public historicalQualityData;
 
-                    function getRecentQualityRecords(address user) 
-                    public view returns (QualityEvent[] memory) {
-                        return recentQualityRecords[user];
-                    }
+    function reportQuality(address user, int256 qualityLevel) public {
+        uint256 currentTimestamp = block.timestamp;
+        QualityEvent memory newEvent = QualityEvent(
+            currentTimestamp,
+            qualityLevel
+        );
+        recentQualityRecords[user].push(newEvent);
+    }
 
-                    function migrateToHistorical(address user, 
-                    uint256 beforeTimestamp) public {
-                        QualityEvent[] storage recentEvents = 
-                        recentQualityRecords[user];
-                        for (uint256 i = 0; i < recentEvents.length; 
-                        i++) {
-                            if (recentEvents[i].timestamp < 
-                            beforeTimestamp) {
-                                historicalQualityData[user].push
-                                (recentEvents[i]);
-                                removeEvent(recentEvents, i);
-                                i--;
-                            }
-                        }
-                    }
+    function getRecentQualityRecords(
+        address user
+    ) public view returns (QualityEvent[] memory) {
+        return recentQualityRecords[user];
+    }
 
-                    function removeEvent(QualityEvent[] storage 
-                    events, uint256 index) internal {
-                        require(index < events.length,
-                        "Index out of bounds");
-                        for (uint256 i = index; i < events.length - 1; 
-                        i++) {
-                            events[i] = events[i + 1];
-                        }
-                        events.pop();
-                    }
-                }
+    function migrateToHistorical(address user, uint256 beforeTimestamp) public {
+        QualityEvent[] storage recentEvents = recentQualityRecords[user];
+        for (uint256 i = 0; i < recentEvents.length; i++) {
+            if (recentEvents[i].timestamp < beforeTimestamp) {
+                historicalQualityData[user].push(recentEvents[i]);
+                removeEvent(recentEvents, i);
+                i--;
+            }
+        }
+    }
+
+    function removeEvent(
+        QualityEvent[] storage events,
+        uint256 index
+    ) internal {
+        require(index < events.length, "Index out of bounds");
+        for (uint256 i = index; i < events.length - 1; i++) {
+            events[i] = events[i + 1];
+        }
+        events.pop();
+    }
+}
